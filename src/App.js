587b487fc072bac052ed75041bd30278b266819f
matthewmichel <mattmichel17@gmail.com>
2021-08-28 14:01:32 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
+import { PaymentCard } from '@crumb-app/crumb-react'
 
 function App() {
+
+  const [paymentSuccessful, setPaymentSuccessful] = useState(false)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          paymentSuccessful ?
+            <>
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </>
+            :
+            <div style={{ width: '50%' }}>
+              <PaymentCard
+                apiKey="Cpwqm1zake5IKEV0L4ivW8KBOmBn4OqOaQo7cYxj"
+                productId="5396ecb3-428e-40cd-b676-c66bed15371c"
+                onSuccess={(res) => {
+                  setPaymentSuccessful(true)
+                }}
+                onError={(err) => {
+                  console.error(err)
+                }}
+              />
+            </div>
+        }
+
       </header>
     </div>
   );
