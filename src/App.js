19dbc89ed2fc08c9d611f11a7f192298b6352594
matthewmichel <mattmichel17@gmail.com>
2021-08-31 22:57:41 -0400

    updating productId and api key

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react'
-
 import { PaymentCard } from '@crumb-app/crumb-react'
+import { useState } from 'react'
 
 function App() {
 
-  const [paymentSuccessful, setPaymentSuccessful] = useState(false)
+  const [paid, setPaid] = useState(false)
 
   return (
     <div className="App">
       <header className="App-header">
         {
-          paymentSuccessful ?
+          paid ?
             <>
               <img src={logo} className="App-logo" alt="logo" />
               <p>
@@ -28,16 +27,16 @@ function App() {
               </a>
             </>
             :
-            <div className="row w-100 flex-row justify-content-center align-items-center">
-              <div className="col-sm-12 col-lg-4">
+            <div className="row w-100 d-flex flex-row justify-content-center">
+              <div className="col-sm-12 col-lg-6">
                 <PaymentCard
-                  apiKey="Cpwqm1zake5IKEV0L4ivW8KBOmBn4OqOaQo7cYxj"
-                  productId="5396ecb3-428e-40cd-b676-c66bed15371c"
-                  onSuccess={(res) => {
-                    setPaymentSuccessful(true)
+                  apiKey="VVaKSzdW7i8vdTmGxzmy1jHQfTpekgt7zf7EriXa"
+                  productId="749c1dde-0f86-4b7a-a6d3-a236973cfd3e"
+                  onSuccess={() => {
+                    setPaid(true)
                   }}
                   onError={(err) => {
-                    console.error(err)
+                    console.log(err)
                   }}
                 />
               </div>
